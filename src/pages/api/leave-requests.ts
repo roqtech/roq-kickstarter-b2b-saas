84944519a637460aadde9301d4c53edb01a6b2f1
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2023-05-02 10:12:08 +0700

    Lint

--- src/pages/api/leave-requests.ts
+++ src/pages/api/leave-requests.ts
@@ -1,124 +1,140 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-import {getServerSession, withAuth} from "@roq/nextjs";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession, withAuth } from '@roq/nextjs'
 import dayjs from 'dayjs'
-import { prisma } from 'server/db';
-import { EmployeeService } from 'server/services/employee.service';
-import { ResourceOperationEnum } from "@roq/nodejs/dist/src/generated/sdk";
-import { retrieveWithAuthorization } from 'library/authorization/retrieve-with-authorization';
-import { AuthorizationForbiddenException } from 'library/authorization/authorization-forbidden.exception';
-import { authorizationClient } from 'server/roq';
+import { prisma } from 'server/db'
+import { EmployeeService } from 'server/services/employee.service'
+import { ResourceOperationEnum } from '@roq/nodejs/dist/src/generated/sdk'
+import { retrieveWithAuthorization } from 'library/authorization/retrieve-with-authorization'
+import { AuthorizationForbiddenException } from 'library/authorization/authorization-forbidden.exception'
+import { authorizationClient } from 'server/roq'
 
 const entity = 'LeaveRequest'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = getServerSession(req)
+  const { roqUserId } = session
 
-    const session = getServerSession(req);
-    const { roqUserId } = session
+  switch (req.method) {
+    case 'GET':
+      return getLeaveRequests()
+    case 'POST':
+      return createLeaveRequest()
+    case 'PATCH':
+      return updateLeaveRequest()
+    // case'DELETE':
+    //     return deleteProject();
+    default:
+      return res
+        .status(405)
+        .json({ message: 'Method ' + req.method + ' not allowed' })
+  }
 
-    switch (req.method) {
-        case 'GET':
-            return getLeaveRequests();
-        case 'POST':
-            return createLeaveRequest();
-        case 'PATCH': 
-          return updateLeaveRequest();
-        // case'DELETE':
-        //     return deleteProject();
-        default:
-            return res.status(405).json({message: 'Method ' + req.method + ' not allowed'})
+  async function getLeaveRequests() {
+    const filter = await authorizationClient.buildAuthorizationFilter(
+      roqUserId,
+      entity,
+    )
+    const data = await prisma.leaveRequest.findMany({
+      where: filter,
+      orderBy: [{ createdAt: 'desc' }],
+      include: {
+        employee: true,
+      },
+    })
+
+    return res.status(200).json({ data })
+  }
+
+  async function createLeaveRequest() {
+    try {
+      const { allowed } = await authorizationClient.hasAccess(
+        roqUserId,
+        entity,
+        ResourceOperationEnum.Create,
+      )
+      if (!allowed) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      const curUser = await prisma.employee.findFirst({ where: { roqUserId } })
+      const data = await prisma.leaveRequest.create({
+        data: {
+          start_date: dayjs().toDate(),
+          end_date: dayjs().add(3, 'day').toDate(),
+          leave_type: 'Sick',
+          status: 'Pending',
+          employee_id: curUser.id,
+        },
+      })
+      return res
+        .status(200)
+        .json({ data, message: 'Create leave request successfully' })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
+  }
 
-
-    async function getLeaveRequests() {
-        const filter = await authorizationClient.buildAuthorizationFilter(roqUserId, entity)
-        const data = await prisma.leaveRequest.findMany({
-            where: filter,
-            orderBy: [{createdAt: 'desc',},],
-            include: {
-              employee: true
-            }
-        });
-
-        return res.status(200).json({ data });
+  async function updateLeaveRequest() {
+    try {
+      const { allowed } = await authorizationClient.hasAccess(
+        roqUserId,
+        entity,
+        ResourceOperationEnum.Update,
+      )
+      if (!allowed) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      if (req.body.status) {
+        // TODO: check role here, > department_manager is allow to update status
+      }
+      const data = await prisma.leaveRequest.update({
+        where: {
+          id: req.body.id,
+        },
+        data: {
+          status: req.body.status,
+          end_date: dayjs()
+            .add(Math.ceil(Math.random() * 10), 'day')
+            .toDate(),
+        },
+      })
+      return res
+        .status(200)
+        .json({ data, message: 'Update leave request successfully' })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
+  }
 
-    async function createLeaveRequest(){
-        try {
-            const { allowed } = await authorizationClient.hasAccess(roqUserId, entity, ResourceOperationEnum.Create)
-            if(!allowed) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            const curUser = await prisma.employee.findFirst({ where: { roqUserId }})
-            const data = await prisma.leaveRequest.create({
-              data: {
-                start_date: dayjs().toDate(),
-                end_date: dayjs().add(3, 'day').toDate(),
-                leave_type: 'Sick',
-                status: 'Pending',
-                employee_id: curUser.id,
-              }
-            });
-            return res.status(200).json({ data, message: 'Create leave request successfully' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: 'Internal server error'});
-        }
+  async function deleteProject() {
+    const id = req.query.id as string
+
+    try {
+      // await retrieveWithAuthorization({
+      //     prismaCollection: prisma.project,
+      //     entity,
+      //     id,
+      //     roqUserId,
+      //     operation: ResourceOperationEnum.Delete
+      // })
+      // const existingWorkspaces = await prisma.workspace.findMany({ where: { projectId: id, }})
+      // if(existingWorkspaces.length > 0) {
+      //     return res.status(200).json({ success: false, message: 'Unable to delete project with existing workspace'})
+      // }
+      // const data = await prisma.project.delete({where: { id: id as string }});
+      const data = {}
+
+      return res.status(200).json({ success: true, data })
+    } catch (error) {
+      if (error instanceof AuthorizationForbiddenException) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      return res.status(500).json({ message: 'Server error' })
     }
-
-    async function updateLeaveRequest(){
-        try {
-            const { allowed } = await authorizationClient.hasAccess(roqUserId, entity, ResourceOperationEnum.Update)
-            if(!allowed) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            if (req.body.status) {
-              // TODO: check role here, > department_manager is allow to update status
-            }
-            const data = await prisma.leaveRequest.update({
-              where: {
-                id: req.body.id
-              },
-              data: {
-                status: req.body.status,
-                end_date: dayjs().add(Math.ceil(Math.random()*10), 'day').toDate(),
-              }
-            });
-            return res.status(200).json({ data, message: 'Update leave request successfully' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: 'Internal server error'});
-        }
-    }
-    
-    async function deleteProject() {
-
-        const id = req.query.id as string;
-
-        try {
-            // await retrieveWithAuthorization({
-            //     prismaCollection: prisma.project,
-            //     entity,
-            //     id,
-            //     roqUserId,
-            //     operation: ResourceOperationEnum.Delete
-            // })
-            // const existingWorkspaces = await prisma.workspace.findMany({ where: { projectId: id, }})
-            // if(existingWorkspaces.length > 0) {
-            //     return res.status(200).json({ success: false, message: 'Unable to delete project with existing workspace'})
-            // }
-            // const data = await prisma.project.delete({where: { id: id as string }});
-            const data = {}
-
-            return res.status(200).json({ success: true, data });
-        } catch (error) {
-            if(error instanceof AuthorizationForbiddenException) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            return res.status(500).json({message: 'Server error'});
-        }
-    }
+  }
 }
 
 // export default handler;
 export default function apiHandler(req: NextApiRequest, res: NextApiResponse) {
-    return withAuth(req, res)(handler);
+  return withAuth(req, res)(handler)
 }
--- src/pages/api/performance-evaluations.ts
+++ src/pages/api/performance-evaluations.ts
@@ -1,133 +1,145 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-import {getServerSession, withAuth} from "@roq/nextjs";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession, withAuth } from '@roq/nextjs'
 import dayjs from 'dayjs'
-import { prisma } from 'server/db';
-import { EmployeeService } from 'server/services/employee.service';
-import { ResourceOperationEnum } from "@roq/nodejs/dist/src/generated/sdk";
-import { retrieveWithAuthorization } from 'library/authorization/retrieve-with-authorization';
-import { AuthorizationForbiddenException } from 'library/authorization/authorization-forbidden.exception';
-import { authorizationClient } from 'server/roq';
+import { prisma } from 'server/db'
+import { EmployeeService } from 'server/services/employee.service'
+import { ResourceOperationEnum } from '@roq/nodejs/dist/src/generated/sdk'
+import { retrieveWithAuthorization } from 'library/authorization/retrieve-with-authorization'
+import { AuthorizationForbiddenException } from 'library/authorization/authorization-forbidden.exception'
+import { authorizationClient } from 'server/roq'
 
 const entity = 'PerformanceEvaluation'
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = getServerSession(req)
+  const { roqUserId } = session
 
-    const session = getServerSession(req);
-    const { roqUserId } = session
+  switch (req.method) {
+    case 'GET':
+      return getPerformanceEvaluations()
+    case 'POST':
+      return createPerformanceEvaluation()
+    case 'PATCH':
+      return updatePerformanceEvaluation()
+    // case'DELETE':
+    //     return deleteProject();
+    default:
+      return res
+        .status(405)
+        .json({ message: 'Method ' + req.method + ' not allowed' })
+  }
 
-    switch (req.method) {
-        case 'GET':
-            return getPerformanceEvaluations();
-        case 'POST':
-            return createPerformanceEvaluation();
-        case 'PATCH':
-            return updatePerformanceEvaluation();
-        // case'DELETE':
-        //     return deleteProject();
-        default:
-            return res.status(405).json({message: 'Method ' + req.method + ' not allowed'})
+  async function getPerformanceEvaluations() {
+    const filter = await authorizationClient.buildAuthorizationFilter(
+      roqUserId,
+      entity,
+    )
+    console.log('getPerformanceEvaluations -> filter:', filter)
+
+    // const curUser = await prisma.employee.findFirst({ where: { roqUserId }})
+    // const department_id = curUser.department_id
+    const data = await prisma.performanceEvaluation.findMany({
+      where: filter,
+      // where: {
+      //   ...filter,
+      // employee: {
+      //   department_id
+      // }
+      // },
+      orderBy: [{ createdAt: 'desc' }],
+      include: {
+        evaluatee: true,
+        evaluator: true,
+      },
+    })
+
+    return res.status(200).json({ data })
+  }
+
+  async function createPerformanceEvaluation() {
+    try {
+      const { allowed } = await authorizationClient.hasAccess(
+        roqUserId,
+        entity,
+        ResourceOperationEnum.Create,
+      )
+      if (!allowed) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      const curUser = await prisma.employee.findFirst({ where: { roqUserId } })
+      const data = await prisma.performanceEvaluation.create({
+        data: {
+          date: dayjs().toDate(),
+          feedback: req.body.feedback ?? 'Good',
+          rating: req.body.rating,
+          evaluator_id: curUser.id,
+          evaluatee_id: req.body.evaluatee_id,
+        },
+      })
+      return res
+        .status(200)
+        .json({ data, message: 'Created evaluation successfully' })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
+  }
 
-
-    async function getPerformanceEvaluations() {
-        const filter = await authorizationClient.buildAuthorizationFilter(roqUserId, entity)
-        console.log('getPerformanceEvaluations -> filter:', filter)
-
-        // const curUser = await prisma.employee.findFirst({ where: { roqUserId }})
-        // const department_id = curUser.department_id
-        const data = await prisma.performanceEvaluation.findMany({
-            where: filter,
-            // where: {
-            //   ...filter, 
-              // employee: {
-              //   department_id
-              // }
-            // },
-            orderBy: [{createdAt: 'desc',},],
-            include: {
-              evaluatee: true,
-              evaluator: true
-            }
-        });
-
-        return res.status(200).json({ data });
+  async function updatePerformanceEvaluation() {
+    try {
+      const { allowed } = await authorizationClient.hasAccess(
+        roqUserId,
+        entity,
+        ResourceOperationEnum.Update,
+      )
+      if (!allowed) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      console.log('updatePerformanceEvaluation -> req.body.id:', req.body.id)
+      const data = await prisma.performanceEvaluation.update({
+        where: {
+          id: req.body.id,
+        },
+        data: {
+          date: dayjs().toDate(),
+          rating: Math.ceil(Math.random() * 10),
+        },
+      })
+      return res.status(200).json({ data, message: 'Update successfully' })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error' })
     }
+  }
 
-    async function createPerformanceEvaluation(){
-        try {
-            const { allowed } = await authorizationClient.hasAccess(roqUserId, entity, ResourceOperationEnum.Create)
-            if(!allowed) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            const curUser = await prisma.employee.findFirst({ where: { roqUserId }})
-            const data = await prisma.performanceEvaluation.create({
-              data: {
-                date: dayjs().toDate(),
-                feedback: req.body.feedback ?? 'Good',
-                rating: req.body.rating,
-                evaluator_id: curUser.id,
-                evaluatee_id: req.body.evaluatee_id
-              }
-            });
-            return res.status(200).json({ data, message: 'Created evaluation successfully' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: 'Internal server error'});
-        }
+  async function deleteProject() {
+    const id = req.query.id as string
+
+    try {
+      // await retrieveWithAuthorization({
+      //     prismaCollection: prisma.project,
+      //     entity,
+      //     id,
+      //     roqUserId,
+      //     operation: ResourceOperationEnum.Delete
+      // })
+      // const existingWorkspaces = await prisma.workspace.findMany({ where: { projectId: id, }})
+      // if(existingWorkspaces.length > 0) {
+      //     return res.status(200).json({ success: false, message: 'Unable to delete project with existing workspace'})
+      // }
+      // const data = await prisma.project.delete({where: { id: id as string }});
+      const data = {}
+
+      return res.status(200).json({ success: true, data })
+    } catch (error) {
+      if (error instanceof AuthorizationForbiddenException) {
+        return res.status(403).json({ message: 'Forbidden' })
+      }
+      return res.status(500).json({ message: 'Server error' })
     }
-
-    async function updatePerformanceEvaluation(){
-        try {
-            const { allowed } = await authorizationClient.hasAccess(roqUserId, entity, ResourceOperationEnum.Update)
-            if(!allowed) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            console.log('updatePerformanceEvaluation -> req.body.id:', req.body.id)
-            const data = await prisma.performanceEvaluation.update({
-              where: {
-                id: req.body.id
-              },
-              data: {
-                date: dayjs().toDate(),
-                rating: Math.ceil(Math.random()*10),
-              }
-            });
-            return res.status(200).json({ data, message: 'Update successfully' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: 'Internal server error'});
-        }
-    }
-
-    async function deleteProject() {
-
-        const id = req.query.id as string;
-
-        try {
-            // await retrieveWithAuthorization({
-            //     prismaCollection: prisma.project,
-            //     entity,
-            //     id,
-            //     roqUserId,
-            //     operation: ResourceOperationEnum.Delete
-            // })
-            // const existingWorkspaces = await prisma.workspace.findMany({ where: { projectId: id, }})
-            // if(existingWorkspaces.length > 0) {
-            //     return res.status(200).json({ success: false, message: 'Unable to delete project with existing workspace'})
-            // }
-            // const data = await prisma.project.delete({where: { id: id as string }});
-            const data = {}
-
-            return res.status(200).json({ success: true, data });
-        } catch (error) {
-            if(error instanceof AuthorizationForbiddenException) {
-                return res.status(403).json({message: 'Forbidden' })
-            }
-            return res.status(500).json({message: 'Server error'});
-        }
-    }
+  }
 }
 
 // export default handler;
 export default function apiHandler(req: NextApiRequest, res: NextApiResponse) {
-    return withAuth(req, res)(handler);
+  return withAuth(req, res)(handler)
 }
